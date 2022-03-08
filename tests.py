6744e1f71dbbbe2d9212efbcaa861d7ac57eb9de
Orr Bezalely <orr.bezalely@gmail.com>
2022-03-08 11:36:06 +0000

    Check password test added

--- tests.py
+++ tests.py
@@ -24,6 +24,15 @@ class PublisherTest(unittest.TestCase):
         self.assertFalse(self.publisher_1.password_hash is None)
         self.assertFalse(self.publisher_2.password_hash is None)
 
+    def test_publisher_check_passwords(self):
+        self.publisher_1.password = "jude1234"
+        self.publisher_2.password = "orr1234"
+
+        self.assertTrue(self.publisher_1.check_password("jude1234"))
+        self.assertTrue(self.publisher_2.check_password("orr1234"))
+        self.assertFalse(self.publisher_1.check_password("klsijlkajsd"))
+        self.assertFalse(self.publisher_2.check_password("lkasjdlkajsf"))
+
 
 if __name__ == "__main__":
     unittest.main()
