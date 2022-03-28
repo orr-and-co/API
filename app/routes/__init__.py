1cd6b9cda05fd43299ebc12b2323938cda9ca37b
jude <judesouthworth@pm.me>
2022-03-28 13:18:27 +0100

    added cors

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,6 +2,14 @@ from flask import Blueprint
 
 api = Blueprint("api", __name__)
 
+
+@api.after_request  # blueprint can also be app~~
+def after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    # Other headers can be added here if required
+    return response
+
+
 from .interests import *
 from .posts import *
 from .publishers import *
